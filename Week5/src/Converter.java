852ad889141373d86d020becd10f4c90f27827a7
stefvnv <stefvnv@yahoo.com>
2022-02-10 23:23:34 +0000

    wtf

--- Week5/src/Converter.java
+++ Week5/src/Converter.java
@@ -1,3 +1,9 @@
 public class Converter {
+
+    public double convertUSDtoEUR(double USD, double xRate) {
+
+        USD = xRate * 1.14116170261326;
+        return USD;
+    }
 }
 
--- Week5/src/ConverterTest.java
+++ Week5/src/ConverterTest.java
@@ -2,4 +2,16 @@ import junit.framework.TestCase;
 
 public class ConverterTest extends TestCase {
 
+    /**
+     * TEST #1
+     *
+     * Objective: Convert USD to EUR
+     * Input(s): USD = "100"
+     * Expected output: "87.43"
+     */
+    public void testConverter001(){
+        Converter testObj = new Converter();
+
+        assertEquals(87.63, testObj.convertUSDtoEUR(100, 1.14116170261326));
+    }
 }
