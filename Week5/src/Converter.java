08594861ac0884d2393d988de85b5887e0414949
Stefana <a00282343@student.ait.ie>
2022-02-14 11:37:33 +0000

    Updated -working week5

--- Week5/src/Converter.java
+++ Week5/src/Converter.java
@@ -1,9 +1,13 @@
 public class Converter {
 
+    //Method signature
     public double convertUSDtoEUR(double USD, double xRate) {
 
-        USD = xRate * 1.14116170261326;
-        return USD;
+        if (USD >= 100 && USD <= 10000 && xRate >= 0.5 && xRate <= 1.6){
+            return USD * xRate;
+
+        }else
+            return -99.00;
     }
 }
 
--- Week5/src/ConverterTest.java
+++ Week5/src/ConverterTest.java
@@ -5,13 +5,41 @@ public class ConverterTest extends TestCase {
     /**
      * TEST #1
      *
-     * Objective: Convert USD to EUR
-     * Input(s): USD = "100"
-     * Expected output: "87.43"
+     * Objective: Invalid values
+     * Input(s): USD = "10", xRate = "0.25"
+     * Expected output: "-99.00"
      */
     public void testConverter001(){
         Converter testObj = new Converter();
 
-        assertEquals(87.63, testObj.convertUSDtoEUR(100, 1.14116170261326));
+        assertEquals(-99.00, testObj.convertUSDtoEUR(10, 0.25));
+    }
+
+
+    /**
+     * TEST #2
+     *
+     * Objective: Valid values
+     * Input(s): USD = "750", xRate = "0.88"
+     * Expected output: "663.81"
+     */
+    public void testConverter002(){
+        Converter testObj = new Converter();
+
+        assertEquals(660.00, testObj.convertUSDtoEUR(750, 0.88), 0.01);
+    }
+
+
+    /**
+     * TEST #3
+     *
+     * Objective: Invalid values
+     * Input(s): USD = "11,750", xRate = "1.00"
+     * Expected output: "87.43"
+     */
+    public void testConverter003(){
+        Converter testObj = new Converter();
+
+        assertEquals(-99.00, testObj.convertUSDtoEUR(11750, 1.00));
     }
 }
